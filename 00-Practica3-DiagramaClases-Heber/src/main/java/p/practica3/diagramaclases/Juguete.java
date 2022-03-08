68fab71c156ceb67a450c2fb969102a1ba38cc8e
Jordi Osarenkhoe Petro <95173613+Jordiop@users.noreply.github.com>
2022-03-08 12:27:30 +0100

    Add files via upload

---
+++ 00-Practica3-DiagramaClases-Heber/src/main/java/p/practica3/diagramaclases/Juguete.java
@@ -0,0 +1,91 @@
+
+package p.practica3.diagramaclases;
+
+/**
+ *
+ * @author jordi
+ */
+public class Juguete {
+    //ATRIBUTOS PRIVADOS
+    private String nombre;
+    private int precio;
+    private int edadRecomendada;
+    
+    //CONSTRUCTOR VACIO
+
+    /**
+     *
+     */
+    public Juguete(){  
+    };
+    
+    //CONSTRUCTOR COMPLETO
+
+    /**
+     *
+     * @param nombre
+     * @param precio
+     * @param edadRecomendada
+     */
+    public Juguete(String nombre,int precio, int edadRecomendada){
+        this.nombre = nombre;
+        this.precio = precio;
+        this.edadRecomendada = edadRecomendada;
+    };
+    
+    //GETTERS && SETTERS
+
+    /**
+     *
+     * @return
+     */
+    public String getNombre(){
+        return nombre;
+    };
+
+    /**
+     *
+     * @param nombre
+     */
+    public void setNombre(String nombre){
+        this.nombre = nombre;
+    };
+    
+    /**
+     *
+     * @return
+     */
+    public int getPrecio(){
+        return precio;
+    };
+
+    /**
+     *
+     * @param precio
+     */
+    public void setPrecio(int precio){
+        this.precio = precio;
+    };
+    
+    /**
+     *
+     * @return
+     */
+    public int getEdadRecomendada(){
+        return edadRecomendada;
+    };
+
+    /**
+     *
+     */
+    public void setEdadRecomendada(){
+        this.edadRecomendada = edadRecomendada;
+    };
+    
+    @Override
+    public String toString(){
+        return "\n [Nombre: "+ this.nombre +"]\n"
+              +" [Precio: "+ this.precio +"]\n"
+              +" [EdadRecomendada: "+ this.edadRecomendada +"]\n";
+    }
+};
